67360d5331f17230601b5c6b683108d0cee99370
yaelschoulal <yschoulal@gmail.com>
2021-03-23 17:35:43 +0000

    rendu

--- c16_03/main.py
+++ c16_03/main.py
@@ -23,15 +23,28 @@ print(moyenne)
 
 #Question 4.B
 
-liste2 = liste[0], liste[2],liste [5]
-print(liste2)
+if __name__ == '__main__':
+    note1 = ('eleve1', 'math', 13)
+    note2 = ('eleve1', 'physique', 15)
+    note3 = ('eleve1', 'math', 13)
+    note4 = ('eleve1', 'eco', 12)
+    note5 = ('eleve1', 'eco', 13)
+    note6 = ('eleve1', 'math', 12)
+    note7 = ('eleve2', 'math', 13)
+    note8 = ('eleve2', 'math', 14)
 
-somme2 = 0
-for i in liste2 :
-   somme2 = somme2 + i
 
-moyenne2 = somme2 / len(liste2) 
-print(moyenne2)
+    notes = [note1, note2, note3, note4, note5, note6,note7,note8]
+
+    notes_elv1 = [note for note in notes if note[0] == 'eleve1']
+
+    print(notes_elv1)
+
+    print(sum(note[2] for note in notes_elv1)/len(notes_elv1))
+
+    notes_elv1_math = [n for n in notes_elv1 if n[1] == 'math']
+
+    print(sum(n[2] for n in notes_elv1_math)/len(notes_elv1_math))
 
 #Question 4.c
 eleve = [elem[0] for elem in notes ] 
@@ -45,10 +58,14 @@ class Note:
     self.eleve = eleve
     self.matiere = matiere
     self.valeur = valeur
-
+ 
 
   def afficher(self):
     print('eleve', self.eleve, 'matiere', self.matiere, 'note', self.valeur)
+
+  def __str__(self):
+    return f"{self.__class__.__name__}('{self.eleve}','{self.matiere}','{self.valeur}')"
+
 
 
 onote = Note('eleve2', 'maths', 13)
@@ -99,3 +116,24 @@ print(onote5bis2.eleve)
 print(onote5bis2.matiere)
 print(onote5bis2.valeur)
 Note.afficher(onote5bis2)
+
+def moyenne_tuple(notes, eleve=None, matiere=None):
+    
+    notes_elv = [note for note in notes if note[0] == eleve] if eleve is not None else notes
+    notes_elv_matiere = [n for n in notes_elv if n[1] == matiere] if matiere is not None else notes_elv
+    return sum([n[2] for n in notes_elv_matiere])/len(notes_elv_matiere)
+
+print(moyenne_tuple(notes, 'eleve1', 'math'))
+print(moyenne_tuple(notes, 'eleve1', 'eco'))
+print(moyenne_tuple(notes, 'eleve2', 'math'))
+print(moyenne_tuple(notes, 'eleve1', 'physique'))
+
+#Question 5:
+liste = [elem[-1] for elem in notes ] 
+print(liste)
+
+onotes = [Note(*note) for note in notes]
+
+#Question 6 :
+
+
